61be6c7a2be348850791c7e4886cebffae8f3bfe
guilebarros <barros.guilherme@gmail.com>
2022-12-04 18:14:14 -0300

    initial commit

---
+++ Source/PluginEditor.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class PFMProject0AudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    PFMProject0AudioProcessorEditor (PFMProject0AudioProcessor&);
+    ~PFMProject0AudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    PFMProject0AudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PFMProject0AudioProcessorEditor)
+};
